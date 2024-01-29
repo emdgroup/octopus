001d098c4e4706fc02e24d1387b7b5560c2577cc
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-01-29 14:01:46 +0000

    new structure for models and their parameters

--- octopus/models/config.py
+++ octopus/models/config.py
@@ -12,14 +12,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
     RandomForestRegressor,
 )
+from sklearn.linear_model import Ridge
 from xgboost import XGBClassifier, XGBRegressor
-
-from octopus.models.extratree import (
-    extratree_class_parameters,
-    extratree_reg_parameters,
-)
-from octopus.models.rf import rf_class_parameters, rf_reg_parameters
-from octopus.models.xgb import xgb_class_parameters, xgb_reg_parameters
 
 model_inventory = {
     "ExtraTreesClassifier": ExtraTreesClassifier,
@@ -28,13 +22,5 @@ model_inventory = {
     "RandomForestRegressor": RandomForestRegressor,
     "XGBRegressor": XGBRegressor,
     "XGBClassifier": XGBClassifier,
-}
-
-parameters_inventory = {
-    "ExtraTreesClassifier": extratree_class_parameters,
-    "ExtraTreesRegressor": extratree_reg_parameters,
-    "RandomForestClassifier": rf_class_parameters,
-    "RandomForestRegressor": rf_reg_parameters,
-    "XGBRegressor": xgb_class_parameters,
-    "XGBClassifier": xgb_reg_parameters,
+    "RidgeRegressor": Ridge,
 }
---
+++ octopus/models/parameters.py
@@ -0,0 +1,123 @@
+"""Model parameters.
+
+"default": default for optuna model parameter settings
+"translate": dictionary to translate global parameters into
+model specific parameters
+"""
+
+# ExtraTree Classifier parameter settings
+extratree_class = {
+    "default": [
+        ("int", {"name": "max_depth", "low": 2, "high": 32}),
+        ("int", {"name": "min_samples_split", "low": 2, "high": 100}),
+        ("int", {"name": "min_samples_leaf", "low": 1, "high": 50}),
+        ("float", {"name": "max_features", "low": 0.1, "high": 1}),
+        ("int", {"name": "n_estimators", "low": 100, "high": 500, "log": False}),
+        ("categorical", {"name": "class_weight", "choices": [None, "balanced"]}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# ExtraTree Regressor parameter settings
+extratree_reg = {
+    "default": [
+        ("int", {"name": "max_depth", "low": 2, "high": 32}),
+        ("int", {"name": "min_samples_split", "low": 2, "high": 100}),
+        ("int", {"name": "min_samples_leaf", "low": 1, "high": 50}),
+        ("float", {"name": "max_features", "low": 0.1, "high": 1}),
+        ("int", {"name": "n_estimators", "low": 100, "high": 500, "log": False}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# Random Forest Classifier parameter settings
+rf_class = {
+    "default": [
+        ("int", {"name": "max_depth", "low": 2, "high": 32}),
+        ("int", {"name": "min_samples_split", "low": 2, "high": 100}),
+        ("int", {"name": "min_samples_leaf", "low": 1, "high": 50}),
+        ("float", {"name": "max_features", "low": 0.1, "high": 1}),
+        ("int", {"name": "n_estimators", "low": 100, "high": 500, "log": False}),
+        ("categorical", {"name": "class_weight", "choices": [None, "balanced"]}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# Random Forest Regressor parameter settings
+rf_reg = {
+    "default": [
+        ("int", {"name": "max_depth", "low": 2, "high": 32}),
+        ("int", {"name": "min_samples_split", "low": 2, "high": 100}),
+        ("int", {"name": "min_samples_leaf", "low": 1, "high": 50}),
+        ("float", {"name": "max_features", "low": 0.1, "high": 1}),
+        ("int", {"name": "n_estimators", "low": 100, "high": 500, "log": False}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# Random Forest Classifier parameter settings
+xgboost_class = {
+    "default": [
+        ("float", {"name": "learning_rate", "low": 1e-4, "high": 0.3, "log": True}),
+        ("int", {"name": "min_child_weight", "low": 2, "high": 15}),
+        ("float", {"name": "subsample", "low": 0.15, "high": 1.0}),
+        ("int", {"name": "n_estimators", "low": 30, "high": 200}),
+        ("int", {"name": "max_depth", "low": 3, "high": 9, "step": 2}),
+        # ("float", {"name": "lambda", "low": 1e-8, "high": 1, "log": True}),
+        # missing: pos_class_weight
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# Random Forest Regressor parameter settings
+xgboost_reg = {
+    "default": [
+        ("float", {"name": "learning_rate", "low": 1e-4, "high": 0.3, "log": True}),
+        ("int", {"name": "min_child_weight", "low": 2, "high": 15}),
+        ("float", {"name": "subsample", "low": 0.15, "high": 1.0}),
+        ("int", {"name": "n_estimators", "low": 30, "high": 200}),
+        ("int", {"name": "max_depth", "low": 3, "high": 9, "step": 2}),
+        # ("float", {"name": "lambda", "low": 1e-8, "high": 1, "log": True}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+# Linear ridge regression parameter settings
+ridge_reg = {
+    "default": [
+        ("float", {"name": "alpha", "low": 1e-2, "high": 1e4, "log": True}),
+        ("categorical", {"name": "fit_intercept", "categories": [True, False]}),
+    ],
+    "translate": {
+        "ml_jobs": "n_jobs",
+        "ml_seed": "random_state",
+    },
+}
+
+parameters_inventory = {
+    "ExtraTreesClassifier": extratree_class,
+    "ExtraTreesRegressor": extratree_reg,
+    "RandomForestClassifier": rf_class,
+    "RandomForestRegressor": rf_reg,
+    "XGBClassifier": xgboost_class,
+    "XGBRegressor": xgboost_reg,
+    "RidgeRegressor": ridge_reg,
+}

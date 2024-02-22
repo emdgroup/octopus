0320660156e00521711017cebff032aa1215a005
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-02-22 10:46:08 +0000

    improve code structure

--- octopus/modules/octo/octofull.py
+++ octopus/modules/octo/octofull.py
@@ -12,9 +12,8 @@ from attrs import define, field, validators
 from optuna.samplers._tpe.sampler import ExperimentalWarning
 
 from octopus.experiment import OctoExperiment
-from octopus.models.parameters import parameters_inventory
-from octopus.models.utils import create_trialparams_from_config
-from octopus.modules.octo.bag import TrainingsBag
+from octopus.modules.octo.bag import Bag
+from octopus.modules.octo.optimize import ObjectiveOptuna
 from octopus.modules.octo.training import Training
 from octopus.modules.utils import optuna_direction
 from octopus.utils import DataSplit
@@ -133,9 +132,9 @@ class OctoFull:
 
         # check if there is a mismatch between configured resources
         # and resources assigned to the experiment
-        self.check_resources()
+        self._check_resources()
 
-    def check_resources(self):
+    def _check_resources(self):
         """Check resources, assigned vs requested."""
         print()
         print("Checking resources:")
@@ -159,18 +158,18 @@ class OctoFull:
     def run_experiment(self):
         """Run experiment."""
         if self.experiment.ml_config["global_hyperparameter"]:
-            self.run_globalhp_optimization()
+            self._run_globalhp_optimization()
         else:
-            self.run_individualhp_optimization()
+            self._run_individualhp_optimization()
 
         # create best bag in results directory
         # - attach best bag to experiment
         # - attach best bag scores to experiment
-        self.create_best_bag()
+        self._create_best_bag()
 
         return self.experiment
 
-    def create_best_bag(self):
+    def _create_best_bag(self):
         """Create best bag from bags found in results.
 
         This code here is only meant to show the desired functionality
@@ -186,16 +185,16 @@ class OctoFull:
             shutil.copy(path_bags[0], self.path_results.joinpath("best_bag.pkl"))
             file = path_bags[0]
             if file.is_file():
-                best_bag = TrainingsBag.from_pickle(file)
+                best_bag = Bag.from_pickle(file)
         elif len(path_bags) > 1:
             # collect all trainings from bags
             trainings = list()
             for file in path_bags:
                 if file.is_file():
-                    bag = TrainingsBag.from_pickle(file)
+                    bag = Bag.from_pickle(file)
                     trainings.extend(bag.trainings)
             # create best bag
-            best_bag = TrainingsBag(
+            best_bag = Bag(
                 trainings=trainings,
                 parallel_execution=self.experiment.ml_config["inner_parallelization"],
                 num_workers=self.experiment.ml_config["n_workers"],
@@ -238,14 +237,14 @@ class OctoFull:
         # save test predictions to experiment
         self.experiment.predictions = best_bag.get_predictions()
 
-    def run_globalhp_optimization(self):
+    def _run_globalhp_optimization(self):
         """Optimization run with a global HP set over all inner folds."""
         print("Running Optuna Optimization with a global HP set")
 
         # run Optuna study with a global HP set
-        self.optimize_splits(self.data_splits)
+        self._optimize_splits(self.data_splits)
 
-    def run_individualhp_optimization(self):
+    def _run_individualhp_optimization(self):
         """Optimization runs with an individual HP set for each inner datasplit."""
         print("Running Optuna Optimizations for each inner datasplit separately")
 
@@ -273,7 +272,7 @@ class OctoFull:
                 futures = []
                 for split in splits:
                     try:
-                        future = executor.submit(self.optimize_splits, split)
+                        future = executor.submit(self._optimize_splits, split)
                         futures.append(future)
                     except Exception as e:  # pylint: disable=broad-except
                         print(f"Exception occurred while submitting task: {e}")
@@ -286,10 +285,10 @@ class OctoFull:
         else:
             print("Sequential execution of Optuna optimizations for individual HPs")
             for split in splits:
-                self.optimize_splits(split)
+                self._optimize_splits(split)
                 print(f"Optimization of split:{split.keys()} completed")
 
-    def optimize_splits(self, splits):
+    def _optimize_splits(self, splits):
         """Optimize splits.
 
         Works if splits contain several splits as well as
@@ -373,7 +372,7 @@ class OctoFull:
                 )
             )
         # create bag with all provided trainings
-        best_bag = TrainingsBag(
+        best_bag = Bag(
             trainings=best_trainings,
             parallel_execution=self.experiment.ml_config["inner_parallelization"],
             num_workers=self.experiment.ml_config["n_workers"],
@@ -407,153 +406,3 @@ class OctoFull:
         #    return self.model.predict_proba(dataset[self.experiment.feature_columns])
         # else:
         #    raise ValueError("predict_proba only supported for classifications")
-
-
-class ObjectiveOptuna:
-    """Callable optuna objective.
-
-    A single solution for global and individual HP optimizations.
-    """
-
-    def __init__(
-        self,
-        experiment,
-        data_splits,
-        study_name,
-        save_trials,
-    ):
-        self.experiment = experiment
-        self.data_splits = data_splits
-        self.study_name = study_name
-        self.save_trials = save_trials
-        # parameters potentially used for optimizations
-        self.ml_model_types = self.experiment.ml_config["models"]
-        self.dim_red_methods = self.experiment.ml_config["dim_red_methods"]
-        self.max_outl = self.experiment.ml_config["max_outl"]
-        # fixed parameters
-        self.ml_seed = self.experiment.ml_config["model_seed"]
-        self.ml_jobs = self.experiment.ml_config["n_jobs"]
-        # training parameters
-        self.parallel_execution = self.experiment.ml_config["inner_parallelization"]
-        self.num_workers = self.experiment.ml_config["n_workers"]
-
-    def __call__(self, trial):
-        """Call.
-
-        We have different types of parameters:
-        (a) non-model parameters that are needed in
-            the training
-        (b) model parameters that are varied by optuna
-            (defined by default or optuna_model_settings)
-        (c) global parameters that have to be translated
-            in fixed model specific parameters
-        """
-        # get non-model parameters
-        # (1) dimension reduction
-        if len(self.dim_red_methods) > 1:
-            dim_reduction = trial.suggest_categorical(
-                name="dim_red_method", choices=self.dim_red_methods
-            )
-        else:
-            dim_reduction = self.dim_red_methods[0]
-
-        # (2) ml_model_type
-        if len(self.ml_model_types) > 1:
-            ml_model_type = trial.suggest_categorical(
-                name="ml_model_type", choices=self.ml_model_types
-            )
-        else:
-            ml_model_type = self.ml_model_types[0]
-
-        # (3) number of outliers to be detected
-        if self.max_outl > 0:
-            num_outl = trial.suggest_int(name="num_outl", low=0, high=self.max_outl)
-        else:
-            num_outl = 0
-
-        # get model parameters
-        optuna_model_settings = None  # use default
-        settings_default = parameters_inventory[ml_model_type]["default"]
-
-        if optuna_model_settings is None:
-            # use default model parameter settings
-            model_params = create_trialparams_from_config(trial, settings_default)
-        else:
-            # use model parameter settings as provided by config
-            model_params = create_trialparams_from_config(trial, optuna_model_settings)
-
-        # overwrite model parameters specified by global settings
-        fixed_global_parameters = {
-            "n_jobs": self.ml_jobs,
-            "model_seed": self.ml_seed,
-        }
-        translate = parameters_inventory[ml_model_type]["translate"]
-        for key, value in fixed_global_parameters.items():
-            if translate[key] != "NA":  # NA=ignore
-                model_params[translate[key]] = value
-
-        config_training = {
-            "dim_reduction": dim_reduction,
-            "outl_reduction": num_outl,
-            "ml_model_type": ml_model_type,
-            "ml_model_params": model_params,
-        }
-
-        # create trainings
-        trainings = list()
-        for key, split in self.data_splits.items():
-            trainings.append(
-                Training(
-                    training_id=self.experiment.id + "_" + str(key),
-                    ml_type=self.experiment.ml_type,
-                    target_assignments=self.experiment.target_assignments,
-                    feature_columns=self.experiment.feature_columns,
-                    row_column=self.experiment.row_column,
-                    data_train=split["train"],  # inner datasplit, train
-                    data_dev=split["test"],  # inner datasplit, dev
-                    data_test=self.experiment.data_test,
-                    config_training=config_training,
-                    target_metric=self.experiment.config["target_metric"],
-                )
-            )
-        # create bag with all provided trainings
-        bag_trainings = TrainingsBag(
-            trainings=trainings,
-            parallel_execution=self.parallel_execution,
-            num_workers=self.num_workers,
-            target_metric=self.experiment.config["target_metric"],
-            row_column=self.experiment.row_column,
-            # path?
-        )
-
-        # train all models in bag
-        bag_trainings.fit()
-
-        # save bag if desired
-        if self.save_trials:
-            path_save = self.experiment.path_study.joinpath(
-                self.experiment.path_sequence_item,
-                "trials",
-                f"study{self.study_name}trial{trial.number}_bag.pkl",
-            )
-            bag_trainings.to_pickle(path_save)
-
-        # evaluate trainings using target metric
-        scores = bag_trainings.get_scores()
-
-        # add scores info to the optuna trial
-        for key, value in scores.items():
-            trial.set_user_attr(key, value)
-
-        # add config_training to user attributes
-        trial.set_user_attr("config_training", config_training)
-
-        # print scores info to console
-        print(f"Trial scores for metric: {self.experiment.config['target_metric']}")
-        for key, value in scores.items():
-            if isinstance(value, list):
-                print(f"{key}:{value}")
-            else:
-                print(f"{key}:{value:.3f}")
-
-        return scores["dev_avg"]  # dev target metric
---
+++ octopus/modules/octo/optimize.py
@@ -0,0 +1,156 @@
+""""Objective function for optuna optimization."""
+
+from octopus.models.parameters import parameters_inventory
+from octopus.models.utils import create_trialparams_from_config
+from octopus.modules.octo.bag import Bag
+from octopus.modules.octo.training import Training
+
+
+class ObjectiveOptuna:
+    """Callable optuna objective.
+
+    A single solution for global and individual HP optimizations.
+    """
+
+    def __init__(
+        self,
+        experiment,
+        data_splits,
+        study_name,
+        save_trials,
+    ):
+        self.experiment = experiment
+        self.data_splits = data_splits
+        self.study_name = study_name
+        self.save_trials = save_trials
+        # parameters potentially used for optimizations
+        self.ml_model_types = self.experiment.ml_config["models"]
+        self.dim_red_methods = self.experiment.ml_config["dim_red_methods"]
+        self.max_outl = self.experiment.ml_config["max_outl"]
+        # fixed parameters
+        self.ml_seed = self.experiment.ml_config["model_seed"]
+        self.ml_jobs = self.experiment.ml_config["n_jobs"]
+        # training parameters
+        self.parallel_execution = self.experiment.ml_config["inner_parallelization"]
+        self.num_workers = self.experiment.ml_config["n_workers"]
+
+    def __call__(self, trial):
+        """Call.
+
+        We have different types of parameters:
+        (a) non-model parameters that are needed in
+            the training
+        (b) model parameters that are varied by optuna
+            (defined by default or optuna_model_settings)
+        (c) global parameters that have to be translated
+            in fixed model specific parameters
+        """
+        # get non-model parameters
+        # (1) dimension reduction
+        if len(self.dim_red_methods) > 1:
+            dim_reduction = trial.suggest_categorical(
+                name="dim_red_method", choices=self.dim_red_methods
+            )
+        else:
+            dim_reduction = self.dim_red_methods[0]
+
+        # (2) ml_model_type
+        if len(self.ml_model_types) > 1:
+            ml_model_type = trial.suggest_categorical(
+                name="ml_model_type", choices=self.ml_model_types
+            )
+        else:
+            ml_model_type = self.ml_model_types[0]
+
+        # (3) number of outliers to be detected
+        if self.max_outl > 0:
+            num_outl = trial.suggest_int(name="num_outl", low=0, high=self.max_outl)
+        else:
+            num_outl = 0
+
+        # get model parameters
+        optuna_model_settings = None  # use default
+        settings_default = parameters_inventory[ml_model_type]["default"]
+
+        if optuna_model_settings is None:
+            # use default model parameter settings
+            model_params = create_trialparams_from_config(trial, settings_default)
+        else:
+            # use model parameter settings as provided by config
+            model_params = create_trialparams_from_config(trial, optuna_model_settings)
+
+        # overwrite model parameters specified by global settings
+        fixed_global_parameters = {
+            "n_jobs": self.ml_jobs,
+            "model_seed": self.ml_seed,
+        }
+        translate = parameters_inventory[ml_model_type]["translate"]
+        for key, value in fixed_global_parameters.items():
+            if translate[key] != "NA":  # NA=ignore
+                model_params[translate[key]] = value
+
+        config_training = {
+            "dim_reduction": dim_reduction,
+            "outl_reduction": num_outl,
+            "ml_model_type": ml_model_type,
+            "ml_model_params": model_params,
+        }
+
+        # create trainings
+        trainings = list()
+        for key, split in self.data_splits.items():
+            trainings.append(
+                Training(
+                    training_id=self.experiment.id + "_" + str(key),
+                    ml_type=self.experiment.ml_type,
+                    target_assignments=self.experiment.target_assignments,
+                    feature_columns=self.experiment.feature_columns,
+                    row_column=self.experiment.row_column,
+                    data_train=split["train"],  # inner datasplit, train
+                    data_dev=split["test"],  # inner datasplit, dev
+                    data_test=self.experiment.data_test,
+                    config_training=config_training,
+                    target_metric=self.experiment.config["target_metric"],
+                )
+            )
+        # create bag with all provided trainings
+        bag_trainings = Bag(
+            trainings=trainings,
+            parallel_execution=self.parallel_execution,
+            num_workers=self.num_workers,
+            target_metric=self.experiment.config["target_metric"],
+            row_column=self.experiment.row_column,
+            # path?
+        )
+
+        # train all models in bag
+        bag_trainings.fit()
+
+        # save bag if desired
+        if self.save_trials:
+            path_save = self.experiment.path_study.joinpath(
+                self.experiment.path_sequence_item,
+                "trials",
+                f"study{self.study_name}trial{trial.number}_bag.pkl",
+            )
+            bag_trainings.to_pickle(path_save)
+
+        # evaluate trainings using target metric
+        scores = bag_trainings.get_scores()
+
+        # add scores info to the optuna trial
+        for key, value in scores.items():
+            trial.set_user_attr(key, value)
+
+        # add config_training to user attributes
+        trial.set_user_attr("config_training", config_training)
+
+        # print scores info to console
+        print(f"Trial scores for metric: {self.experiment.config['target_metric']}")
+        for key, value in scores.items():
+            if isinstance(value, list):
+                print(f"{key}:{value}")
+            else:
+                print(f"{key}:{value:.3f}")
+
+        return scores["dev_avg"]  # dev target metric

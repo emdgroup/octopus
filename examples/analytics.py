27a0c9e6b8a8e98d5bc69d15aa5674e100d02b83
M228393 <nils.haase@merckgroup.com>
2024-03-05 17:26:41 +0100

    add optuna page

--- examples/analytics.py
+++ examples/analytics.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from octopus.analytics.run import OctoAnalitics
 
-folder_path = Path("studies").joinpath("2024_02_27_htm_octofull_chtm_EQE")
+folder_path = Path("studies").joinpath("2024_02_27_htm_octofull_chtm_EQE_2")
 
 octo_ana = OctoAnalitics(folder_path)
 

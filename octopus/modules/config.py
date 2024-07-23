5dbf95cfd3bfb18442a7b6c89962acdd413b22b2
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-07-23 21:14:07 +0000

    ROC module

--- octopus/modules/config.py
+++ octopus/modules/config.py
@@ -8,6 +8,7 @@ except ImportError:
 from octopus.modules.mrmr import Mrmr
 from octopus.modules.octo.octofull import OctoFull
 from octopus.modules.rfe import RfeModule
+from octopus.modules.roc import RocModule
 from octopus.modules.sfs import SfsModule
 
 # inventory for all available modules
@@ -17,4 +18,5 @@ modules_inventory = {
     "mrmr": Mrmr,
     "rfe": RfeModule,
     "sfs": SfsModule,
+    "roc": RocModule,
 }

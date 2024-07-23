0c6400856636c9f39e126c75b0e7acffd830c619
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-07-23 13:57:29 +0000

    added test scripts

--- workflow_mrmr_after_mbos6_octofull.py
+++ workflow_mrmr_after_mbos6_octofull.py
@@ -3,17 +3,13 @@
 import os
 import socket
 
-import attrs
-
 # OPENBLASE config needs to be before pandas, autosk
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["OMP_NUM_THREADS"] = "1"
+# os.environ["OPENBLAS_NUM_THREADS"] = "1"
+# os.environ["OMP_NUM_THREADS"] = "1"
+import pandas as pd
 
-import pandas as pd  # noqa E402
-
-from octopus import OctoConfig, OctoData, OctoML  # noqa E402
-from octopus.modules.mrmr import MrmrConfig  # noqa E402
-from octopus.modules.octo.config import OctopusFullConfig  # noqa E402
+from octopus import OctoData, OctoML
+from octopus.config import ConfigManager, ConfigSequence, ConfigStudy, MrmrConfig, Octo
 
 print("Notebook kernel is running on server:", socket.gethostname())
 print("Conda environment on server:", os.environ["CONDA_DEFAULT_ENV"])
@@ -46,138 +42,149 @@ stratification_column = ["STRAT_OS6_TRT_NUM"]
 # pre-process data
 print("Number of samples with target values:", len(data[target_column]))
 
-# define data_input
-data_input = {
-    "data": data,
-    "sample_id": sample_column,
-    "target_columns": target_column,
-    "datasplit_type": "sample",
-    "feature_columns": features,
-    "stratification_column": stratification_column,
-}
+### Create OctoData Object
 
-# create OctoData object
-data = OctoData(**data_input)
-
-# configure study
-config_study = {
-    "study_name": "20240322A_MBOS6_octofull_5x5_ETREE",
-    "output_path": "./studies/",
-    "production_mode": False,
-    "start_with_empty_study": False,
-    "ml_type": "classification",
-    "n_folds_outer": 5,
-    "target_metric": "AUCROC",
-    "metrics": ["AUCROC"],
-    "datasplit_seed_outer": 1234,
-}
-
-# configure manager
-config_manager = {
-    # outer loop
-    "outer_parallelization": True,
-    # only run specific single experiment, for quick testing
-    # "run_single_experiment_num": 4,
-}
-
-# define processing sequence
-sequence_item_1 = OctopusFullConfig(
-    description="step1_octofull",
-    # loading of existing results
-    load_sequence_item=True,
-    # datasplit
-    n_folds_inner=5,
-    datasplit_seed_inner=0,
-    # model training
-    models=[
-        "ExtraTreesClassifier",
-        # "RandomForestClassifier",
-        # "CatBoostClassifier",
-        # "XGBClassifier",
-    ],
-    model_seed=0,
-    n_jobs=1,
-    dim_red_methods=[""],
-    max_outl=0,
-    fi_methods_bestbag=["permutation"],
-    # parallelization
-    inner_parallelization=True,
-    n_workers=5,
-    # HPO
-    optuna_seed=0,
-    n_optuna_startup_trials=10,
-    resume_optimization=False,
-    global_hyperparameter=True,
-    n_trials=400,
-    max_features=70,
-    penalty_factor=1.0,
+# We define the data, target columns, feature columns, sample ID to identify groups,
+# and the data split type. For this classification approach,
+# we also define a stratification column.
+octo_data = OctoData(
+    data=data,
+    target_columns=target_column,
+    feature_columns=features,
+    sample_id=sample_column,
+    datasplit_type="sample",
+    stratification_column=stratification_column,
 )
 
+### Create Configuration
 
-# define processing sequence
-sequence_item_2 = MrmrConfig(
-    description="step2_mrmr",
-    # number of features selected by MRMR
-    n_features=50,
-    # what correlation type should be used
-    correlation_type="rdc",
-    # feature importance type (mean/count)
-    feature_importance_type="mean",
-    # feature importance method (permuation/shap/internal)
-    feature_importance_method="permutation",
+# We create three types of configurations:
+# 1. `ConfigStudy`: Sets the name, machine learning type (classification),
+# and target metric.
+
+# 2. `ConfigManager`: Manages how the machine learning will be executed.
+# We use the default settings.
+
+# 3. `ConfigSequence`: Defines the sequences to be executed. In this example,
+# we use one sequence with the `RandomForestClassifier` model.
+
+config_study = ConfigStudy(
+    name="MBOS6_test",
+    ml_type="classification",
+    target_metric="AUCROC",
+    metrics=["AUCROC", "ACCBAL", "ACC", "LOGLOSS"],
+    datasplit_seed_outer=1234,
+    n_folds_outer=5,
+    start_with_empty_study=True,
+    path="./studies/",
 )
 
-
-# define processing sequence
-sequence_item_3 = OctopusFullConfig(
-    description="step2_octofull",
-    # loading of existing results
-    load_sequence_item=False,
-    # datasplit
-    n_folds_inner=5,
-    datasplit_seed_inner=0,
-    # model training
-    models=[
-        # "TabPFNClassifier",
-        "ExtraTreesClassifier",
-        # "RandomForestClassifier",
-        # "CatBoostClassifier",
-        # "XGBClassifier",
-    ],
-    model_seed=0,
-    n_jobs=1,
-    dim_red_methods=[""],
-    max_outl=0,
-    # fi_methods_bestbag=["permutation"],
-    # parallelization
-    inner_parallelization=True,
-    n_workers=5,
-    # HPO
-    optuna_seed=0,
-    n_optuna_startup_trials=10,
-    resume_optimization=False,
-    global_hyperparameter=True,
-    n_trials=30,
-    # max_features=70,
-    # penalty_factor=1.0,
+config_manager = ConfigManager(
+    # outer loop parallelization
+    outer_parallelization=True,
+    # only process first outer loop experiment, for quick testing
+    run_single_experiment_num=1,
+    production_mode=False,
 )
-# config_sequence = [
-#    attrs.asdict(sequence_item_1),
-# ]
 
-config_sequence = [
-    attrs.asdict(sequence_item_1),
-    attrs.asdict(sequence_item_2),
-    attrs.asdict(sequence_item_3),
-]
-# create study config
-octo_config = OctoConfig(config_manager, config_sequence, **config_study)
+config_sequence = ConfigSequence(
+    [
+        # Step1: octo
+        Octo(
+            description="step_1_octo",
+            # loading of existing results
+            load_sequence_item=True,
+            # datasplit
+            n_folds_inner=5,
+            # model selection
+            models=[
+                # "TabPFNClassifier",
+                "ExtraTreesClassifier",
+                # "RandomForestClassifier",
+                # "CatBoostClassifier",
+                # "XGBClassifier",
+            ],
+            model_seed=0,
+            n_jobs=1,
+            dim_red_methods=[""],
+            max_outl=0,
+            fi_methods_bestbag=["permutation"],
+            # parallelization
+            inner_parallelization=True,
+            n_workers=5,
+            # HPO
+            optuna_seed=0,
+            n_optuna_startup_trials=10,
+            resume_optimization=False,
+            global_hyperparameter=True,
+            n_trials=40,
+            max_features=70,
+            penalty_factor=1.0,
+        ),
+        # Step2: MRMR
+        MrmrConfig(
+            description="step2_mrmr",
+            # number of features selected by MRMR
+            n_features=50,
+            # what correlation type should be used
+            correlation_type="rdc",
+            # feature importance type (mean/count)
+            feature_importance_type="mean",
+            # feature importance method (permuation/shap/internal)
+            feature_importance_method="permutation",
+        ),
+        # Step3: Octo
+        Octo(
+            description="step2_octofull",
+            # loading of existing results
+            # load_sequence_item=False,
+            # datasplit
+            n_folds_inner=5,
+            datasplit_seed_inner=0,
+            # model training
+            models=[
+                # "TabPFNClassifier",
+                "ExtraTreesClassifier",
+                # "RandomForestClassifier",
+                # "CatBoostClassifier",
+                # "XGBClassifier",
+            ],
+            model_seed=0,
+            n_jobs=1,
+            dim_red_methods=[""],
+            max_outl=0,
+            # fi_methods_bestbag=["permutation"],
+            # parallelization
+            inner_parallelization=True,
+            n_workers=5,
+            # HPO
+            optuna_seed=0,
+            n_optuna_startup_trials=10,
+            resume_optimization=False,
+            global_hyperparameter=True,
+            n_trials=30,
+            # max_features=70,
+            # penalty_factor=1.0,
+        ),
+    ]
+)
 
-# create ML object
-oml = OctoML(data, octo_config)
+### Execute the Machine Learning Workflow
 
-oml.create_outer_experiments()
-
-oml.run_outer_experiments()
+# We add the data and the configurations defined earlier
+# and run the machine learning workflow.
+octo_ml = OctoML(
+    octo_data,
+    config_study=config_study,
+    config_manager=config_manager,
+    config_sequence=config_sequence,
+)
+octo_ml.create_outer_experiments()
+octo_ml.run_outer_experiments()
 
 print("Workflow completed")
+
+# This completes the basic example for using Octopus Classification
+# with the Titanic dataset. The workflow involves loading and preprocessing
+# the data, creating necessary configurations, and executing the machine
+# learning pipeline.

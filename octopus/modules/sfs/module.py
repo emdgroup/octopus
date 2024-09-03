1505a663ff630a00d105875a5ba525de7a967a33
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-09-03 22:24:07 +0000

    outlier detection + LogisticRegression added

--- octopus/modules/sfs/module.py
+++ octopus/modules/sfs/module.py
@@ -1,6 +1,6 @@
 """SFS Module (sequential feature selection)."""
 
-from attrs import define, field, validators
+from attrs import Factory, define, field, validators
 
 from octopus.config.base_sequence_item import BaseSequenceItem
 
@@ -13,9 +13,10 @@ class Sfs(BaseSequenceItem):
     """Module name."""
 
     load_sequence_item: bool = field(
-        init=False, validator=validators.instance_of(bool), default=False
+        validator=validators.instance_of(bool),
+        default=Factory(lambda: False),
     )
-    """Load existing sequence item, fixed, set to False"""
+    """Load existing sequence item. Default is False"""
 
     model: str = field(validator=[validators.instance_of(str)], default="")
     """Model used by SFS."""
@@ -29,22 +30,6 @@ class Sfs(BaseSequenceItem):
                 ["forward", "backward", "floating_forward", "floating_backward"]
             )
         ],
-        default="floating_backward",
+        default="backward",
     )
     """Sfs type used."""
-
-    # correlation_type: str = field(
-    #    validator=[validators.in_(["pearson", "rdc"])], default="pearson"
-    # )
-    # """Selection of correlation type."""
-    #
-    # feature_importance_type: str = field(
-    #    validator=[validators.in_(["mean", "count"])], default="mean"
-    # )
-    # """Selection of feature importance type."""
-    #
-    # feature_importance_method: str = field(
-    #    validator=[validators.in_(["permutation", "shap", "internal"])],
-    #    default="permutation",
-    # )
-    # """Selection of feature importance method."""

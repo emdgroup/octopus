8ad7a312b522e1a2f60d04b14e25aa4520c1d55b
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-09-06 11:21:59 +0000

    workflow fix

--- examples/test_scripts/workflow_mbos6_3steps_sfs.py
+++ examples/test_scripts/workflow_mbos6_3steps_sfs.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 from octopus import OctoData, OctoML
 from octopus.config import ConfigManager, ConfigSequence, ConfigStudy
-from octopus.modules import Mrmr, Octo, Roc, Sfs
+from octopus.modules import Mrmr, Octo, Sfs
 
 print("Notebook kernel is running on server:", socket.gethostname())
 print("Conda environment on server:", os.environ["CONDA_DEFAULT_ENV"])

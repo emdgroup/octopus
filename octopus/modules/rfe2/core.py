22fc13e0fe481f146aac5df7876ec49cd4355f16
Andreas Wurl <andreas.wurl@merckgroup.com>
2024-09-26 21:49:50 +0000

    first rfe2 steps, enable efs

---
+++ octopus/modules/rfe2/core.py
@@ -0,0 +1,51 @@
+"""RFE2 core function."""
+
+from attrs import define
+
+from octopus.modules.octo.core import OctoCore
+
+# RF2 TOBEDONE:
+#  - train bag in standard octo way
+#  - give bag and hyperparameters to rfe-process
+#  - (start) train bag und standard hyperparameters
+#  - calculate group_pfi and shap
+# - remove least important feature
+#   -- automatically remove not used features
+#   -- deal with group features, needs to be intelligent
+#   -- deal with negative feature importances
+#   -- consider count information
+# - record performance
+# - go back to start and repeat
+# - select the model, different approaches
+#   -- persimonial
+#   -- best model
+# - model retraining after n removal, or start use module several times
+# - autogluon: add 3-5 random feature and remove alle feature below the lowest random
+# - rewrite using iheritance from OctoCore, overwrite run_experiment
+
+
+@define
+class Rfe2Core(OctoCore):
+    """Rfe2 Core."""
+
+    @property
+    def config(self) -> dict:
+        """Module configuration."""
+        return self.experiment.ml_config
+
+    def run_experiment(self):
+        """Run experiment."""
+        # (1) train and optimize model
+        self._optimize_splits(self.data_splits)
+
+        # create best bag in results directory
+        # - attach best bag to experiment
+        # - attach best bag scores to experiment
+        self._create_best_bag()
+
+        # (2) run RFE
+        print("config:", self.config)
+
+        # model should be found here: self.experiment.results["best"]
+
+        return self.experiment
---
+++ octopus/modules/rfe2/core_old.py
@@ -0,0 +1,358 @@
+"""OctoFull core function."""
+
+# import concurrent.futures
+import copy
+import json
+import shutil
+import warnings
+from pathlib import Path
+
+import optuna
+from attrs import Factory, define, field, validators
+from optuna.samplers._tpe.sampler import ExperimentalWarning
+
+from octopus.experiment import OctoExperiment
+from octopus.modules.octo.bag import Bag
+from octopus.modules.octo.objective_optuna import ObjectiveOptuna
+from octopus.modules.octo.training import Training
+from octopus.results import ModuleResults
+from octopus.utils import DataSplit
+
+# ignore three Optuna experimental warnings
+# !may be specific to optuna version due to line number
+for line in [319, 330, 338]:
+    warnings.filterwarnings(
+        "ignore",
+        category=ExperimentalWarning,
+        module="optuna.samplers._tpe.sampler",
+        lineno=line,
+    )
+
+
+# RF2 TOBEDONE:
+#  - train bag in standard octo way
+#  - give bag and hyperparameters to rfe-process
+#  - (start) train bag und standard hyperparameters
+#  - calculate group_pfi and shap
+# - remove least important feature
+#   -- automatically remove not used features
+#   -- deal with group features, needs to be intelligent
+#   -- deal with negative feature importances
+#   -- consider count information
+# - record performance
+# - go back to start and repeat
+# - select the model, different approaches
+#   -- persimonial
+#   -- best model
+# - model retraining after n removal, or start use module several times
+# - autogluon: add 3-5 random feature and remove alle feature below the lowest random
+# - rewrite using iheritance from OctoCore, overwrite run_experiment
+
+
+@define
+class Rfe2Core:
+    """Rfe3 Core."""
+
+    experiment: OctoExperiment = field(
+        validator=[validators.instance_of(OctoExperiment)]
+    )
+    # model = field(default=None)
+    data_splits: dict = field(
+        default=Factory(dict), validator=[validators.instance_of(dict)]
+    )
+
+    paths_optuna_db: dict = field(
+        default=Factory(dict), validator=[validators.instance_of(dict)]
+    )
+
+    top_trials: list = field(
+        default=Factory(list), validator=[validators.instance_of(list)]
+    )
+
+    mrmr_features: dict = field(
+        default=Factory(dict), validator=[validators.instance_of(dict)]
+    )
+
+    @property
+    def path_module(self) -> Path:
+        """Module path."""
+        return self.experiment.path_study.joinpath(self.experiment.path_sequence_item)
+
+    @property
+    def path_optuna(self) -> Path:
+        """Optuna db path."""
+        return self.path_module.joinpath("optuna")
+
+    @property
+    def path_trials(self) -> Path:
+        """Trials path."""
+        return self.path_module.joinpath("trials")
+
+    @property
+    def path_results(self) -> Path:
+        """Results path."""
+        return self.path_module.joinpath("results")
+
+    def __attrs_post_init__(self):
+        # create datasplit during init
+        self.data_splits = DataSplit(
+            dataset=self.experiment.data_traindev,
+            datasplit_col=self.experiment.datasplit_column,
+            seed=self.experiment.ml_config.datasplit_seed_inner,
+            num_folds=self.experiment.ml_config.n_folds_inner,
+            stratification_col=self.experiment.stratification_column,
+        ).get_datasplits()
+        # if we don't want to resume optimization:
+        # delete directories /trials /optuna /results to ensure clean state
+        # of module when restarted, required for parallel optuna runs
+        # as optuna.create(...,load_if_exists=True)
+        # create directory if it does not exist
+        for directory in [self.path_optuna, self.path_trials, self.path_results]:
+            if not self.experiment.ml_config.resume_optimization:
+                if directory.exists():
+                    shutil.rmtree(directory)
+            directory.mkdir(parents=True, exist_ok=True)
+
+        # check if there is a mismatch between configured resources
+        # and resources assigned to the experiment
+        self._check_resources()
+
+        # only use original features
+        self.mrmr_features[len(self.experiment.feature_columns)] = (
+            self.experiment.feature_columns
+        )
+
+    def _check_resources(self):
+        """Check resources, assigned vs requested."""
+        print()
+        print("Checking resources:")
+        print(
+            "Number of CPUs available to this experiment:",
+            self.experiment.num_assigned_cpus,
+        )
+
+        # assuming n_jobs=1 for every model
+        if self.experiment.ml_config.inner_parallelization is True:
+            num_requested_cpus = (
+                self.experiment.ml_config.n_workers * self.experiment.ml_config.n_jobs
+            )
+        else:
+            num_requested_cpus = self.experiment.ml_config.n_jobs
+
+        print(f"Number of requested CPUs for this experiment: {num_requested_cpus}")
+        print()
+
+    def run_experiment(self):
+        """Run experiment."""
+        # (1) train and optimize model
+        self._optimize_splits(self.data_splits)
+
+        # create best bag in results directory
+        # - attach best bag to experiment
+        # - attach best bag scores to experiment
+        self._create_best_bag()
+
+        # (2) run RFE
+
+        return self.experiment
+
+    def _create_best_bag(self):
+        """Create best bag from bags found in results.
+
+        This code here is only meant to show the desired functionality
+        and needs to be improved.
+        It shows an indirect way of creating the best bag. It
+        would be preferable to access the optuna results and
+        then create the best bag from them.
+        """
+        path_bags = list(self.path_results.rglob("*.pkl"))
+
+        if len(path_bags) == 1:
+            # only single bag found - copy to best bag
+            shutil.copy(path_bags[0], self.path_results.joinpath("best_bag.pkl"))
+            file = path_bags[0]
+            if file.is_file():
+                best_bag = Bag.from_pickle(file)
+        elif len(path_bags) > 1:
+            # collect all trainings from bags
+            trainings = list()
+            for file in path_bags:
+                if file.is_file():
+                    bag = Bag.from_pickle(file)
+                    trainings.extend(bag.trainings)
+            # create best bag
+            best_bag = Bag(
+                bag_id=self.experiment.id + "_best",
+                trainings=trainings,
+                target_assignments=self.experiment.target_assignments,
+                parallel_execution=self.experiment.ml_config.inner_parallelization,
+                num_workers=self.experiment.ml_config.n_workers,
+                target_metric=self.experiment.config.target_metric,
+                row_column=self.experiment.row_column,
+            )
+            # save best bag
+            best_bag.to_pickle(self.path_results.joinpath("best_bag.pkl"))
+        else:
+            raise ValueError("No bags founds in results directory")
+
+        # save performance values of best bag
+        best_bag_scores = best_bag.get_scores()
+        # show and save test results
+        print(
+            f"Experiment: {self.experiment.id} "
+            f"{self.experiment.configs.study.target_metric} "
+            f"(ensembled, hard vote):"  # noqa E501
+            f"Dev {best_bag_scores['dev_pool_hard']:.3f}, "
+            f"Test {best_bag_scores['test_pool_hard']:.3f}"
+        )
+        if self.experiment.ml_type == "classification":
+            print(
+                f"Experiment: {self.experiment.id} "
+                f"{self.experiment.configs.study.target_metric} "
+                f"(ensembled, soft vote):"  # noqa E501
+                f"Dev {best_bag_scores['dev_pool_soft']:.3f}, "
+                f"Test {best_bag_scores['test_pool_soft']:.3f}"
+            )
+
+        with open(
+            self.path_results.joinpath("best_bag_scores.json"), "w", encoding="utf-8"
+        ) as f:
+            json.dump(best_bag_scores, f)
+
+        # calculate feature importances of best bag
+        fi_methods = self.experiment.ml_config.fi_methods_bestbag
+        best_bag_fi = best_bag.get_feature_importances(fi_methods)
+
+        # calculate selected features
+        selected_features = best_bag.get_selected_features(fi_methods)
+
+        # save best bag and results to experiment
+        self.experiment.results["best"] = ModuleResults(
+            id="best",
+            model=best_bag,
+            scores=best_bag_scores,
+            feature_importances=best_bag_fi,
+            selected_features=selected_features,
+            predictions=best_bag.get_predictions(),
+        )
+
+        # save selected features to experiment
+        print("Number of original features:", len(self.experiment.feature_columns))
+        self.experiment.selected_features = selected_features
+        print("Number of selected features:", len(self.experiment.selected_features))
+        if len(self.experiment.selected_features) == 0:
+            print(
+                "Warning: All feature importances values are zero. Therefore, no "
+                "features could be selected. This hints at a model related problem."
+            )
+
+    def _optimize_splits(self, splits):
+        """Optimize splits.
+
+        Works if splits contain several splits as well as
+        when splits only contains a single split
+        """
+        # define study name by joined keys of splits
+        study_name = "optuna_" + str(sum([int(key) for key in splits.keys()]))
+
+        # set up Optuna study
+        objective = ObjectiveOptuna(
+            experiment=self.experiment,
+            data_splits=splits,
+            study_name=study_name,
+            top_trials=self.top_trials,
+            mrmr_features=self.mrmr_features,
+        )
+
+        # multivariate sampler with group option
+        sampler = optuna.samplers.TPESampler(
+            multivariate=True,
+            group=True,
+            constant_liar=True,
+            seed=self.experiment.ml_config.optuna_seed,
+            n_startup_trials=self.experiment.ml_config.n_optuna_startup_trials,
+        )
+
+        # create study with unique name and database
+        db_path = self.path_optuna.joinpath(study_name + ".db")
+        storage = optuna.storages.RDBStorage(url=f"sqlite:///{db_path}")
+        study = optuna.create_study(
+            study_name=study_name,
+            direction="minimize",  # metric adjustment in optuna objective
+            sampler=sampler,
+            storage=storage,
+            load_if_exists=True,
+        )
+        # store optuna db path
+        self.paths_optuna_db[study_name] = db_path
+
+        study.optimize(
+            objective,
+            n_jobs=1,
+            n_trials=self.experiment.ml_config.n_trials,
+        )
+
+        # copy bag of best trial to results
+        # not needed anymore
+        # source = self.experiment.path_study.joinpath(
+        #    self.experiment.path_sequence_item,
+        #    "trials",
+        #    f"study{study_name}trial{study.best_trial.number}_bag.pkl",
+        # )
+        # shutil.copy(source, self.path_results / source.name)
+
+        # display results
+        print()
+        print("Optimization results: ")
+        # print("Best trial:", study.best_trial) #full info
+        print("Best trial number:", study.best_trial.number)
+        print("Best target value:", study.best_value)
+        user_attrs = study.best_trial.user_attrs
+        performance_info = {
+            key: v for key, v in user_attrs.items() if key not in ["config_training"]
+        }
+        print("Best parameters:", user_attrs["config_training"])
+        print("Performance Info:", performance_info)
+        print("Optimization completed")
+
+        # create best bag from optuna info
+        best_trainings = list()
+        for key, split in splits.items():
+            best_trainings.append(
+                Training(
+                    training_id=self.experiment.id + "_" + str(key),
+                    ml_type=self.experiment.ml_type,
+                    target_assignments=self.experiment.target_assignments,
+                    feature_columns=self.experiment.feature_columns,
+                    row_column=self.experiment.row_column,
+                    data_train=split["train"],  # inner datasplit, train
+                    data_dev=split["test"],  # inner datasplit, dev
+                    data_test=self.experiment.data_test,
+                    config_training=user_attrs["config_training"],
+                    target_metric=self.experiment.configs.study.target_metric,
+                    max_features=self.experiment.ml_config.max_features,
+                    feature_groups=self.experiment.feature_groups,
+                )
+            )
+        # create bag with all provided trainings
+        best_bag = Bag(
+            bag_id=self.experiment.id + "_best",
+            trainings=best_trainings,
+            target_assignments=self.experiment.target_assignments,
+            parallel_execution=self.experiment.ml_config.inner_parallelization,
+            num_workers=self.experiment.ml_config.n_workers,
+            target_metric=self.experiment.configs.study.target_metric,
+            row_column=self.experiment.row_column,
+            # path?
+        )
+
+        # train all models in best_bag
+        best_bag.fit()
+        # save best bag
+        best_bag.to_pickle(
+            self.path_results.joinpath(
+                f"{study_name}_trial{study.best_trial.number}_bag.pkl"
+            )
+        )
+
+        return True

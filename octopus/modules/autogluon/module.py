4021ee07d8881350321f399eb91c40f31b35daab
Andreas Wurl <andreas.wurl@merckgroup.com>
2025-01-31 20:28:20 +0000

    autogluon predictions

--- octopus/modules/autogluon/module.py
+++ octopus/modules/autogluon/module.py
@@ -1,5 +1,7 @@
 """AutoGluon module."""
 
+from typing import ClassVar, List, Literal, Optional, Union
+
 from attrs import Factory, define, field, validators
 
 from octopus.config.base_sequence_item import BaseSequenceItem
@@ -9,20 +11,51 @@ from octopus.config.base_sequence_item import BaseSequenceItem
 class AutoGluon(BaseSequenceItem):
     """AG Config."""
 
-    module: str = field(default="ag")
+    module: ClassVar[str] = "autogluon"
     """Module name."""
 
-    description: str = field(validator=[validators.instance_of(str)], default="")
+    description: Optional[str] = field(
+        default="", validator=validators.instance_of(str)
+    )
     """Description."""
 
-    load_sequence_item: bool = field(
-        validator=validators.instance_of(bool),
-        default=Factory(lambda: False),
+    time_limit: Optional[int] = field(
+        default=None, validator=validators.optional(validators.instance_of(int))
     )
-    """Load existing sequence item. Default is False"""
+    """Approximately, how long a fit should run, in seconds. Default: No limit."""
 
-    model: str = field(validator=[validators.instance_of(str)], default="")
-    """Model used by AG."""
+    presets: List[str] = field(
+        default=Factory(lambda: ["medium_quality"]),
+        validator=validators.deep_iterable(
+            member_validator=validators.and_(
+                validators.instance_of(str),
+                validators.in_(
+                    [
+                        "best_quality",
+                        "high_quality",
+                        "good_quality",
+                        "medium_quality",
+                        "experimental_quality",
+                        "optimize_for_deployment",
+                        "interpretable",
+                        "ignore_text",
+                    ]
+                ),
+            ),
+            iterable_validator=validators.instance_of(list),
+        ),
+    )
+    """Autogluon presets."""
 
-    # cv: int = field(validator=[validators.instance_of(int)], default=5)
-    # """Number of CV folds."""
+    num_cpus: Union[int, Literal["auto"]] = field(
+        default="auto",
+        validator=validators.optional(
+            validators.or_(validators.instance_of(int), validators.in_(["auto"]))
+        ),
+    )
+    """Number of CPUs used by Autogluon instance. Can be an integer or "auto"."""
+
+    num_bag_folds: int = field(
+        default=5, validator=[validators.instance_of(int), validators.gt(1)]
+    )
+    """Number of cross validation folds."""

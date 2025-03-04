30c6f496e369d48e290bb59b0be0cd1b65d9dfcd
Nils Haase <nils.haase@merckgroup.com>
2025-03-04 08:51:26 +0100

    remove dashboard code

--- octopus/__init__.py
+++ octopus/__init__.py
@@ -2,11 +2,7 @@
 
 import sys
 
-from octopus.dashboard.run import OctoDash  # noqa: F401
 from octopus.data.core import OctoData  # noqa: F401
-
-# from octopus.experiment import OctoExperiment  # noqa: F401
-# from octopus.manager import OctoManager  # noqa: F401
 from octopus.ml import OctoML  # noqa: F401
 
 if not sys.version_info >= (3, 12):

56d31d99ed8da55f9c84930b87aaa88ab6d8b78a
Nils Haase <nils.haase@merckgroup.com>
2025-03-10 17:40:06 +0100

    pre commit

--- octopus/metrics/regression.py
+++ octopus/metrics/regression.py
@@ -55,15 +55,3 @@ class MSEMetric:
 
 
 __all__ = ["R2Metric", "MAEMetric", "MSEMetric"]
-
-
-# # Constants for metric names
-# MAE = "MAE"
-# MSE = "MSE"
-# R2 = "R2"
-
-# regression_metrics = {
-#     MAE: mean_absolute_error,
-#     MSE: mean_squared_error,
-#     R2: r2_score,
-# }

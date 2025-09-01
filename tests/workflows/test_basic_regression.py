5901e8944f9fa823374353bce6956a83377c2dbe
Mathias Winkel <mathias.winkel@merckgroup.com>
2025-09-01 11:23:20 +0200

    automatically download data sets for workflow tests

--- tests/workflows/test_basic_regression.py
+++ tests/workflows/test_basic_regression.py
@@ -1,80 +1,75 @@
 """Test basic regression workflow."""
 
-# import os
+from sklearn.datasets import fetch_california_housing
 
-# import pandas as pd
-
-# from octopus import OctoData, OctoML
-# from octopus.config import ConfigManager, ConfigSequence, ConfigStudy
-# from octopus.modules import Octo
+from octopus import OctoData, OctoML
+from octopus.config import ConfigManager, ConfigSequence, ConfigStudy
+from octopus.modules import Octo
 
 
-# def test_basic_regression():
-#     """Test basic regression."""
-#     data_df = (
-#         pd.read_csv(
-#             os.path.join(os.getcwd(), "datasets", "california_housing_prices.csv")
-#         )
-#         .reset_index()
-#         .astype(
-#             {
-#                 "housing_median_age": int,
-#                 "total_rooms": int,
-#                 "population": int,
-#                 "households": int,
-#                 "median_income": int,
-#                 "median_house_value": int,
-#             }
-#         )
-#         .loc[0:100, :]
-#     )
+def test_basic_regression():
+    """Test basic regression."""
+    data_df = (
+        fetch_california_housing(as_frame=True)
+        .frame.reset_index()
+        .astype(
+            {
+                "HouseAge": int,
+                "AveRooms": float,
+                "AveBedrms": float,
+                "Population": int,
+                "AveOccup": float,
+                "MedInc": float,
+                "MedHouseVal": float,
+            }
+        )
+        .loc[0:100, :]
+    )
 
-#     octo_data = OctoData(
-#         data=data_df,
-#         target_columns=["median_house_value"],
-#         feature_columns=[
-#             "longitude",
-#             "latitude",
-#             "housing_median_age",
-#             "total_rooms",
-#             "population",
-#             "households",
-#             "median_income",
-#         ],
-#         sample_id="index",
-#         datasplit_type="sample",
-#     )
+    octo_data = OctoData(
+        data=data_df,
+        target_columns=["MedHouseVal"],
+        feature_columns=[
+            "Longitude",
+            "Latitude",
+            "HouseAge",
+            "AveRooms",
+            "AveBedrms",
+            "Population",
+            "AveOccup",
+            "MedInc",
+        ],
+        sample_id="index",
+        datasplit_type="sample",
+    )
 
-#     config_study = ConfigStudy(
-#         name="test_basic_regression",
-#         ml_type="regression",
-#         target_metric="R2",
-#         silently_overwrite_study=True,
-#         ignore_data_health_warning=True,
-#     )
+    config_study = ConfigStudy(
+        name="test_basic_regression",
+        ml_type="regression",
+        target_metric="R2",
+        silently_overwrite_study=True,
+        ignore_data_health_warning=True,
+    )
 
-#     config_manager = ConfigManager(
-#         outer_parallelization=False, run_single_experiment_num=0
-#     )
+    config_manager = ConfigManager(outer_parallelization=False, run_single_experiment_num=0)
 
-#     config_sequence = ConfigSequence(
-#         [
-#             Octo(
-#                 sequence_id=0,
-#                 input_sequence_id=-1,
-#                 description="step_1",
-#                 models=["RandomForestRegressor", "XGBRegressor"],
-#                 n_trials=1,
-#             ),
-#         ]
-#     )
-
-#     octo_ml = OctoML(
-#         octo_data,
-#         config_study=config_study,
-#         config_manager=config_manager,
-#         config_sequence=config_sequence,
-#     )
-#     octo_ml.run_study()
-#     success = True
-#     assert success is True
+    config_sequence = ConfigSequence(
+        [
+            Octo(
+                sequence_id=0,
+                input_sequence_id=-1,
+                description="step_1",
+                models=["RandomForestRegressor", "XGBRegressor"],
+                n_trials=1,
+            ),
+        ]
+    )
+    octo_ml = OctoML(
+        octo_data,
+        config_study=config_study,
+        config_manager=config_manager,
+        config_sequence=config_sequence,
+    )
+    octo_ml.run_study()
+    success = True
+    assert success is True

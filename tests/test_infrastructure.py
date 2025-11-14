42d6ec2d3bb83790cb3f7a67fa51c50e40eef70f
Mathias Winkel <mathias.winkel@merckgroup.com>
2025-11-14 16:22:41 +0100

    fix: omp num threads, resolves #163, resolves #137
    This ensure that OMP_NUM_THREADS and related environment variables are set to '1' if they are unspecified.
    
    If they are specified externally, we emit a warning message
    * about potential crashes on macOS
    * about potential performance issues on other OS
    but do not attempt to change the variables to allow the user to perform scaling studies and evaluate threading performance themselves.
    
    In addition, we use threadpoolctl to limit the number of threads used by thread pools in libraries such as OpenBLAS, MKL, BLIS, Veclib, and NumExpr to 1.
    
    Two unit tests ensure these settings survive and are applied.
    
    Furthermore, we let every ray worker test whether parallelization has really been turned off.
    If this is not the case, an error is raised to prevent oversubscription.
    This is also being tested using a unit test that spawns a subprocess with OMP_NUM_THREADS set to 42.
    
    Fixes #163
    Fixes #137

---
+++ tests/test_infrastructure.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+import sys
+
+import threadpoolctl
+
+import octopus  # noqa: F401
+from octopus.modules import _PARALLELIZATION_ENV_VARS
+
+
+def test_parallelization_inactive_in_threadpoolctl():
+    threadpool_info = threadpoolctl.threadpool_info()
+
+    # we expect the following openmp libraries to be loaded: shipped with torch, shipped with sklearn, system openmp
+    assert len(threadpool_info) >= 3
+
+    for lib in threadpool_info:
+        assert lib["num_threads"] == 1
+
+
+def test_parallelization_limited_by_env():
+    # these vars are being set in octopus/modules/__init__.py
+    for env_var in _PARALLELIZATION_ENV_VARS:
+        assert os.environ.get(env_var, None) == "1"
+
+
+def test_ray_workers_detect_active_parallelization():
+    """Test that ray workers abort if they detect active parallelization.
+
+    This test spawns a subprocess that runs a test workflow with OMP_NUM_THREADS set to 42
+    to make sure the modification of the ENV var is not accidentally carried over to
+    other tests.
+    """
+    env = os.environ.copy()
+    env["OMP_NUM_THREADS"] = "42"  # Activate thread-level parallelization
+
+    res = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pytest",
+            "tests/workflows/test_ag_workflows.py",
+            "-k",
+            "full_regression_workflow",
+        ],
+        check=False,
+        env=env,
+        capture_output=True,
+    )
+
+    assert res.returncode == 1  # Expecting failure due to active thread-level parallelization
+    assert b"RuntimeError: Environment variable OMP_NUM_THREADS is set to 42." in res.stdout
